d7062731ec1819d5a92c1618c6d6aebb8f94c585
Damian Gałka <galka.damian.91@gmail.com>
2017-05-07 20:52:13 +0200

    #1 : Add class to retrieve the paragraphs

---
+++ converters/src/main/java/pl/edu/uj/gauee/converters/Html2Paragraph.java
@@ -0,0 +1,17 @@
+package pl.edu.uj.gauee.converters;
+
+import java.util.Collections;
+import java.util.List;
+import javax.swing.text.html.HTML;
+
+/**
+ * Created by gauee on 5/7/17.
+ */
+public class Html2Paragraph {
+
+  public List<String> retrieveParagraphs(HTML html) {
+    return Collections.emptyList();
+  }
+
+
+}
